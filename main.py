14ef3180df16c905d3f43641e6465fdd91597dda
Burak Türker <pythonu0@gmail.com>
2023-07-03 13:55:35 +0300

    The overall design is complete.

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("BMI Calculator")
-window.minsize(width=300, height=400)
+window.minsize(width=300, height=300,)
+
+height_label = tkinter.Label(text="Boyunuzu giriniz: (cm)")
+height_label.pack(pady=0)
 
 height_entry = tkinter.Entry(width=30)
-height_entry.pack()
+height_entry.focus()
+height_entry.pack(pady=15)
 
+width_label = tkinter.Label(text="Kilonuzu giriniz: (kg)")
+width_label.pack(pady=0)
 width_entry = tkinter.Entry(width=30)
-width_entry.pack()
+width_entry.pack(pady=15)
 
-result_button = tkinter.Button(width=20)
-result_button.pack()
+result_button = tkinter.Button(width=20, text="result")
+result_button.pack(padx=20, pady=15)
 
 result_label = tkinter.Label(text="deneme deneme deneme")
 result_label.pack()
