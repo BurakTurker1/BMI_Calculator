9725074904c93e62ca03dd7f9d2974a4c8c665f0
Burak Türker <pythonu0@gmail.com>
2023-07-03 15:23:23 +0300

    calculation completed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter
-
+BMI_result=0
 window = tkinter.Tk()
 window.title("BMI Calculator")
 window.minsize(width=300, height=300,)
@@ -16,9 +16,24 @@ width_label.pack(pady=0)
 width_entry = tkinter.Entry(width=30)
 width_entry.pack(pady=15)
 
-result_button = tkinter.Button(width=20, text="result")
+def clickButton():
+    user_width = float(width_entry.get())
+    user_height = float(height_entry.get()) / 100
+    BMI_result = user_width / (user_height * user_height)
+    result_label.config(text=f"Bmı değeriniz: {BMI_result:.2f}")
+    if BMI_result<18.5:
+        result_label.config(text=f"Bmı değeriniz: {BMI_result:.2f} zayıf")
+    elif BMI_result>19 and BMI_result<24.9:
+        result_label.config(text=f"Bmı değeriniz: {BMI_result:.2f} sağlıklı kilo")
+    elif BMI_result>25 and BMI_result<29.9:
+        result_label.config(text=f"Bmı değeriniz: {BMI_result:.2f} kilolu")
+    elif BMI_result>30:
+        result_label.config(text=f"Bmı değeriniz: {BMI_result:.2f} obez")
+
+
+result_button = tkinter.Button(width=20, text="result", command=clickButton)
 result_button.pack(padx=20, pady=15)
 
-result_label = tkinter.Label(text="deneme deneme deneme")
+result_label = tkinter.Label()
 result_label.pack()
 window.mainloop()
